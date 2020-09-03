8b3a63f63a74c4fae854d851ace917968d619563
taotao313 <1913475244@qq.com>
2020-09-03 20:25:59 +0800

    '添加单例模式'

---
+++ designmode/src/test/java/com/lsy/createdmeode/singlemode/demo02/Singleton_07Test.java
@@ -0,0 +1,18 @@
+package com.lsy.createdmeode.singlemode.demo02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 测试枚举
+ *
+ * @author TT
+ */
+public class Singleton_07Test {
+    @Test
+    public void testSinglenton() {
+        Singleton_07.INSTANCE.test();
+    }
+
+}
